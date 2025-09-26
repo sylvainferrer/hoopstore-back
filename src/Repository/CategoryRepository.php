be5fa0be7590e754783d57012be033be00b00562
Sylvain <40465096+sylvainferrer@users.noreply.github.com>
2025-09-26 15:42:15 +0200

    update_26092025

--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -19,17 +19,6 @@ class CategoryRepository extends ServiceEntityRepository
     //    /**
     //     * @return Category[] Returns an array of Category objects
     //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
 
     //    public function findOneBySomeField($value): ?Category
     //    {
